5c5cb30b52ef74db002d68c420a24a8d2d347eb9
kelvin Musselli <kelvinmusselli22@gmail.com>
2022-04-22 19:35:18 -0300

    feat: implements all tests

--- src/components/NewTransactionModal/index.test.tsx
+++ src/components/NewTransactionModal/index.test.tsx
@@ -9,33 +9,6 @@ jest.mock('../../services/api');
 jest.mock('react-modal', () => ({ children }) => children);
 
 describe('<NewTransactionModal>', () => {
-
-  beforeEach(() => {
-    jest.resetAllMocks();
-    jest.clearAllMocks();
-    api.get.mockImplementation(() => {
-      return Promise.resolve({
-        data: {
-          transactions:[]
-        }
-      });
-    });
-    api.post.mockImplementation(() => {
-      return Promise.resolve({
-        data: {
-          transactions:[{
-            id: 1,
-            title: 'Cadastrar',
-            type: 'deposit',
-            value: 100,
-            category: 'Alimentação',
-            created_at: '2020-01-01T00:00:00.000Z',
-          }]
-        }
-      });
-    });
-  });
-
   const renderComponent = (props) => {
     const mockProps = {
       ...props,
@@ -47,22 +20,101 @@ describe('<NewTransactionModal>', () => {
       </TransactionsProvider>
     );
   };
-  it('Should render correctly', async () => {
-    renderComponent({ isOpen: true });
-    await waitFor(() => {
-      expect(screen.getByText('Cadastrar transação')).toBeInTheDocument();
-    });
 
-    const title = screen.getByPlaceholderText('Título');
-    const amount = screen.getByPlaceholderText('Valor');
-    const category = screen.getByPlaceholderText('Categoria');
-    const submit = screen.getByRole('button', { name: 'Cadastrar' });
-    fireEvent.change(title, { target: { value: 'Nova transação' } });
-    fireEvent.change(amount, { target: { value: '100' } });
-    fireEvent.change(category, { target: { value: 'Alimentação' } });
-    fireEvent.submit(submit);
-    await waitFor(() => {
-      expect(api.post).toHaveBeenCalled();
+  describe('Transaction deposit', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+      jest.clearAllMocks();
+      api.get.mockImplementation(() => {
+        return Promise.resolve({
+          data: {
+            transactions:[]
+          }
+        });
+      });
+      api.post.mockImplementation(() => {
+        return Promise.resolve({
+          data: {
+            transactions:[{
+              id: 1,
+              title: 'Cadastrar',
+              type: 'deposit',
+              value: 100,
+              category: 'Alimentação',
+              created_at: '2020-01-01T00:00:00.000Z',
+            }]
+          }
+        });
+      });
+    });
+    it('Add new transaction deposit', async () => {
+      renderComponent({ isOpen: true });
+      await waitFor(() => {
+        expect(screen.getByText('Cadastrar transação')).toBeInTheDocument();
+      });
+
+      const title = screen.getByPlaceholderText('Título');
+      const amount = screen.getByPlaceholderText('Valor');
+      const deposit = screen.getByTestId('deposit');
+      const category = screen.getByPlaceholderText('Categoria');
+      const submit = screen.getByRole('button', { name: 'Cadastrar' });
+      fireEvent.change(title, { target: { value: 'Nova transação' } });
+      fireEvent.change(amount, { target: { value: '100' } });
+      fireEvent.change(category, { target: { value: 'Alimentação' } });
+      fireEvent.click(deposit);
+      fireEvent.submit(submit);
+      await waitFor(() => {
+        expect(api.post).toHaveBeenCalled();
+      });
     });
   });
+
+  describe('Transaction withdraw', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+      jest.clearAllMocks();
+      api.get.mockImplementation(() => {
+        return Promise.resolve({
+          data: {
+            transactions:[]
+          }
+        });
+      });
+      api.post.mockImplementation(() => {
+        return Promise.resolve({
+          data: {
+            transactions:[{
+              id: 1,
+              title: 'Cadastrar',
+              type: 'withdraw',
+              value: 100,
+              category: 'Alimentação',
+              created_at: '2020-01-01T00:00:00.000Z',
+            }]
+          }
+        });
+      });
+    });
+    it('Add new transaction deposit', async () => {
+      renderComponent({ isOpen: true });
+      await waitFor(() => {
+        expect(screen.getByText('Cadastrar transação')).toBeInTheDocument();
+      });
+
+      const title = screen.getByPlaceholderText('Título');
+      const amount = screen.getByPlaceholderText('Valor');
+      const deposit = screen.getByTestId('withdraw');
+      const category = screen.getByPlaceholderText('Categoria');
+      const submit = screen.getByRole('button', { name: 'Cadastrar' });
+      fireEvent.change(title, { target: { value: 'Nova transação' } });
+      fireEvent.change(amount, { target: { value: '100' } });
+      fireEvent.change(category, { target: { value: 'Alimentação' } });
+      fireEvent.click(deposit);
+      fireEvent.submit(submit);
+      await waitFor(() => {
+        expect(api.post).toHaveBeenCalled();
+      });
+    });
+  });
+
 });
